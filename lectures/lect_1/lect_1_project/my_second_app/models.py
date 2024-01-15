cdc31a9e2b9eb0f0ea6f3095805c94d0afa40fc0
Alex <Evlampyev.Alex@gmail.com>
2024-01-15 17:37:47 +0500

    lect_2 continue

--- lectures/lect_1/lect_1_project/my_second_app/models.py
+++ lectures/lect_1/lect_1_project/my_second_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)  # два знака после запятой
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')  # сами картинки мы будем хранить в папке product/
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2)
