0050ab5b056b2f467b84fe53f6451ebb7fb82b67
Alex <35335834+Evlampyev@users.noreply.github.com>
2024-01-16 00:52:33 +0500

    Sem_2 ended

--- seminars/sem_1/sem_1_project/secondapp/urls.py
+++ seminars/sem_1/sem_1_project/secondapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import heads_and_tails, cube_faces, random_digits
+from .views import heads_and_tails, cube_faces, random_digits, ht_results, authors_view, \
+    posts_view
 
 urlpatterns = [
-    path('/heads_and_tails', heads_and_tails, name='heads_and_tails'),
-    path('/cube_faces', cube_faces, name='cube_faces'),
-    path('/random_digits', random_digits, name='random_digits'),
+    path('heads_and_tails', heads_and_tails, name='heads_and_tails'),
+    path('cube_faces', cube_faces, name='cube_faces'),
+    path('random_digits', random_digits, name='random_digits'),
+    path('heads_and_tails/result', ht_results, name='ht_result'),
+    path('authors', authors_view, name='authors_view'),
+    path('posts', posts_view, name='posts_view'),
 ]
--- seminars/sem_1/sem_1_project/secondapp/views.py
+++ seminars/sem_1/sem_1_project/secondapp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from random import choice, randint
 from logging import getLogger
+from .models import Money, Author, Post
+from datetime import datetime
+import json
 
 # Create your views here.
 
@@ -11,8 +14,17 @@ logger = getLogger(__name__)
 def heads_and_tails(request):
     logger.info("Орел и решка страница")
     lst = ['орел', 'решка']
+    situation = choice(lst)
+    time = datetime.now()
+    money = Money(situation=situation, time_created=time)
+    money.save()
+    return HttpResponse(situation)
 
-    return HttpResponse(choice(lst))
+
+def ht_results(request):
+    result = Money.get_data(5)
+
+    return HttpResponse(str(result))
 
 
 def cube_faces(request):
@@ -23,3 +35,15 @@ def cube_faces(request):
 def random_digits(request):
     logger.info("Случайное число от 1 до 100")
     return HttpResponse(randint(1, 100))
+
+
+def authors_view(request):
+    authors = Author.objects.all()
+    res_str = '<br>'.join([str(author) for author in authors])
+    return HttpResponse(str(res_str))
+
+
+def posts_view(request):
+    posts = Post.objects.all()
+    res_str = '<br>'.join([str(post) for post in posts])
+    return HttpResponse(res_str)
